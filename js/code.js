36110e945a6400a36b564c30c0254a2507f66887
Przemysław Matkowski <matkowski1994@gmail.com>
2019-09-02 16:22:27 +0200

    added gallery, update content of section

--- js/code.js
+++ js/code.js
@@ -19,12 +19,15 @@ const scrollToSection = () => {
   btnServices = document.querySelector('nav  ul .services');
   btnPrice = document.querySelector('nav  ul .price');
   btnAbout = document.querySelector('nav  ul .about');
+  btnGallery = document.querySelector('nav  ul .gallery');
   btnContact = document.querySelector('nav  ul .contact');
   home1 = document.querySelector('.home1');
   homeSize = document.querySelector('div.home').offsetTop;
   servicesSize = document.querySelector('div.services').offsetTop;
   priceSize = document.querySelector('div.price').offsetTop;
+
   aboutSize = document.querySelector('div.about').offsetTop;
+  gallerySize = document.querySelector('div.gallery').offsetTop;
   contactSize = document.querySelector('div.contact').offsetTop;
 
 
@@ -45,6 +48,10 @@ const scrollToSection = () => {
     window.scrollTo(0, aboutSize);
 
   })
+  btnGallery.addEventListener('click', function () {
+    window.scrollTo(0, gallerySize);
+
+  })
   btnContact.addEventListener('click', function () {
     window.scrollTo(0, contactSize);
 
@@ -58,21 +65,24 @@ const sectionArea = () => {
   services = document.querySelector('div.services').clientHeight;
   price = document.querySelector('div.price').clientHeight;
   about = document.querySelector('div.about').clientHeight;
+  gallery = document.querySelector('div.gallery').clientHeight;
   contact = document.querySelector('div.contact').clientHeight;
   btnHome = document.querySelector('nav  ul .home');
   btnServices = document.querySelector('nav  ul .services');
   btnPrice = document.querySelector('nav  ul .price');
   btnAbout = document.querySelector('nav  ul .about');
+  btnGallery = document.querySelector('nav  ul .gallery');
   btnContact = document.querySelector('nav  ul .contact');
   const spaceBetweemSecondAndFirst = document.querySelector('div.services').offsetTop;
   const spaceBetweemSecondAndSecond = document.querySelector('div.price').offsetTop;
   const spaceBetweemSecondAndThird = document.querySelector('div.about').offsetTop;
-  const spaceBetweemSecondAndFourth = document.querySelector('div.contact').offsetTop;
+  const spaceBetweemSecondAndFourth = document.querySelector('div.gallery').offsetTop;
   const scrollSize = window.scrollY;
   if (scrollSize < home) {
     //add
     btnHome.classList.add('active');
     //remove
+    btnGallery.classList.remove('active');
     btnServices.classList.remove('active');
     btnPrice.classList.remove('active');
     btnAbout.classList.remove('active');
@@ -82,6 +92,7 @@ const sectionArea = () => {
     //add
     btnServices.classList.add('active');
     //remove
+    btnGallery.classList.remove('active');
     btnHome.classList.remove('active');
     btnPrice.classList.remove('active');
     btnAbout.classList.remove('active');
@@ -91,6 +102,7 @@ const sectionArea = () => {
     //add
     btnPrice.classList.add('active');
     //remove
+    btnGallery.classList.remove('active');
     btnHome.classList.remove('active');
     btnServices.classList.remove('active');
     btnAbout.classList.remove('active');
@@ -99,15 +111,26 @@ const sectionArea = () => {
     //add
     btnAbout.classList.add('active');
     //remove
+    btnGallery.classList.remove('active');
+    btnHome.classList.remove('active');
+    btnServices.classList.remove('active');
+    btnPrice.classList.remove('active');
+    btnContact.classList.remove('active');
+  } else if (scrollSize < gallery + spaceBetweemSecondAndFourth) {
+    //add
+    btnGallery.classList.add('active');
+
+    //remove
+    btnAbout.classList.remove('active');
     btnHome.classList.remove('active');
     btnServices.classList.remove('active');
     btnPrice.classList.remove('active');
     btnContact.classList.remove('active');
   } else {
-
     //add
     btnContact.classList.add('active');
     //remove
+    btnGallery.classList.remove('active');
     btnHome.classList.remove('active');
     btnServices.classList.remove('active');
     btnPrice.classList.remove('active');
@@ -137,13 +160,17 @@ const addYear = () => {
 addYear();
 
 
-const btnOffert = () => {
-  const btn = document.querySelectorAll('.price button');
-  btn.forEach((b) => {
-    b.addEventListener('click', () => {
-      window.scrollTo(0, servicesSize);
+// const btnOffert = () => {
+//   const btn = document.querySelectorAll('.price button');
+//   btn.forEach((b) => {
+//     b.addEventListener('click', () => {
+//       window.scrollTo(0, servicesSize);
 
-    })
-  })
-}
-btnOffert();
+//     })
+//   })
+// }
+// btnOffert();
+
+// $('#myModal').on('shown.bs.modal', function () {
+//   $('#myInput').trigger('focus')
+// })
